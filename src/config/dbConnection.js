d1a7dd27d093456b1632c6c06b15dd4167b84487
DGKN <sakardogukan@gmail.com>
2023-11-21 16:56:42 +0300

    Added files in config folder

---
+++ src/config/dbConnection.js
@@ -0,0 +1,16 @@
+"use strict"
+/* --------------------- DB CONNECTION --------------- */
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const dbConnection = function () {
+    mongoose.connect((process.env?.MONGODB || 'mongodb://127.0.0.1:27017/pizzaApi'))
+        .then(() => console.log(' * DB Connected * '))
+        .catch((err) => console.log(' * DB Not Connected * ', err))
+}
+
+module.exports = {
+    mongoose,
+    dbConnection
+}
