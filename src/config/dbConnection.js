c15341432b550fe78bc67b55074126f25d930952
DGKN <sakardogukan@gmail.com>
2023-11-25 21:52:51 +0300

    dbConnection.js config file changed

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,11 +1,10 @@
 "use strict"
 /* --------------------- DB CONNECTION --------------- */
 require('dotenv').config()
-
 const mongoose = require('mongoose')
 
 const dbConnection = function () {
-    mongoose.connect((process.env?.MONGODB || 'mongodb://127.0.0.1:27017/pizzaApi'))
+    mongoose.connect((process.env?.MONGODB || 'mongodb://127.0.0.1:27017/pizzaApiApp'))
         .then(() => console.log(' * DB Connected * '))
         .catch((err) => console.log(' * DB Not Connected * ', err))
 }
