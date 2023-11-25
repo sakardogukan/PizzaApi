2c56716fa293f204486fd1c5d579873fa2ba32b9
DGKN <sakardogukan@gmail.com>
2023-11-25 21:43:42 +0300

    topping.js changed

--- src/routes/topping.js
+++ src/routes/topping.js
@@ -1,3 +1,20 @@
 "use strict"
-/* ------------------- TOPPING ROUTE ------------------- */
+/* ---------------- TOPPING ROUTE --------------- */
 
+const router = require('express').Router()
+const topping = require('../controllers/topping')
+const permissions = require('../middlewares/permissions')
+
+router.use(permissions.isAdmin)
+
+router.route('/')
+    .get(topping.list)
+    .post(topping.create)
+
+router.route('/:id')
+    .get(topping.read)
+    .put(topping.update)
+    .patch(topping.update)
+    .delete(topping.delete)
+
+module.exports = router
