8a24d28fd2eff2d4eebef16c2f82fc6389aed905
DGKN <sakardogukan@gmail.com>
2023-11-25 21:43:09 +0300

    order.js changed

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,2 +1,20 @@
 "use strict"
-/* --------------------- ORDER ROUTE ------------------- */
+/* ---------------- ORDER ROUTE --------------- */
+
+const router = require('express').Router()
+const order = require('../controllers/order')
+const permissions = require('../middlewares/permissions')
+
+router.use(permissions.isLogin)
+
+router.route('/')
+    .get(order.list)
+    .post(order.create)
+
+router.route('/:id')
+    .get(order.read)
+    .put(order.update)
+    .patch(order.update)
+    .delete(permissions.isAdmin, order.delete)
+
+module.exports = router
