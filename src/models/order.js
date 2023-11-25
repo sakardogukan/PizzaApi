86ffa6687e5f8e8316036923ed3a34899a538b6f
DGKN <sakardogukan@gmail.com>
2023-11-25 21:45:26 +0300

    order.js changed

--- src/models/order.js
+++ src/models/order.js
@@ -1,2 +1,42 @@
 "use strict"
-/* --------------------- ORDER MODEL ------------------- */
+/* ---------------- ORDER MODEL --------------- */
+
+const { mongoose } = require('../config/dbConnection')
+
+const OrderSchema = new mongoose.Schema({
+    //id autoCreated
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User ID in Order filed must be required!']
+    },
+    pizzaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pizza',
+        required: [true, 'Pizza ID in Order filed must be required!']
+    },
+    size: {
+        type: String,
+        required: true,
+        enum: ['Small', 'Medium', 'Large']
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 1.0
+    },
+    totalPrice: {
+        type: Number,
+    }
+
+}, {
+    collection: 'orders',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
