ff924319e49630756ae96db63876f3734c68b2ed
DGKN <sakardogukan@gmail.com>
2023-11-25 21:44:51 +0300

    topping.js changed

--- src/models/topping.js
+++ src/models/topping.js
@@ -1,3 +1,20 @@
 "use strict"
-/* ------------------- TOPPING MODEL ------------------- */
+/* ---------------- TOPPING MODEL --------------- */
 
+const { mongoose } = require('../config/dbConnection')
+
+const ToppingSchema = new mongoose.Schema({
+    //id autoCreated
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
+
+}, {
+    collection: 'toppings',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Topping', ToppingSchema)
