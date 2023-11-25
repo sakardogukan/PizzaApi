f11215b0ee91943449c8a2c14da82d3e562f41ce
DGKN <sakardogukan@gmail.com>
2023-11-25 21:45:06 +0300

    pizza.js changed

--- src/models/pizza.js
+++ src/models/pizza.js
@@ -1,2 +1,34 @@
 "use strict"
-/* --------------------- PIZZA MODEL ------------------- */
+/* ---------------- PIZZA MODEL --------------- */
+
+const { mongoose } = require('../config/dbConnection')
+
+const PizzaSchema = new mongoose.Schema({
+    //id autoCreated
+    name: {
+        type: String,
+        trim: true,
+        unique: [true, 'There is this Pizzaname. Pizza name filed must be unique'],
+        required: [true, 'Pizza Name filed must be required!']
+    },
+    image: {
+        type: String,
+        trim: true
+    },
+    price: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+    toppings: [ // push, pull
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Topping'
+        }
+    ]
+}, {
+    collection: 'pizzas',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Pizza', PizzaSchema)
