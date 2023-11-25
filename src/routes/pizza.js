2027df0bbb5a24e0ab8eff70d4e93bde7fd64e08
DGKN <sakardogukan@gmail.com>
2023-11-25 21:43:25 +0300

    pizza.js changed

--- src/routes/pizza.js
+++ src/routes/pizza.js
@@ -1,2 +1,21 @@
 "use strict"
-/* --------------------- PIZZA ROUTE ------------------- */
+/* ---------------- PIZZA ROUTE --------------- */
+
+const router = require('express').Router()
+const permissions = require('../middlewares/permissions')
+const pizza = require('../controllers/pizza')
+
+router.route('/')
+    .get(pizza.list)
+    .post(permissions.isAdmin, pizza.create)
+
+router.route('/:id')
+    .get(pizza.read)
+    .put(permissions.isAdmin, pizza.update)
+    .patch(permissions.isAdmin, pizza.update)
+    .delete(permissions.isAdmin, pizza.delete)
+
+router.put('/:id/pushToppings', permissions.isAdmin, pizza.pushToppings)
+router.put('/:id/pullToppings', permissions.isAdmin, pizza.pullToppings)
+
+module.exports = router
