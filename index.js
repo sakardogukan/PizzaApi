4f20d8198edf574a9f9d31c841139ae3a8c19b09
DGKN <sakardogukan@gmail.com>
2023-11-25 23:09:20 +0300

    index.js file changed

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ require('dotenv').config({ path: __dirname + '/.env' })
 // require('dotenv').config()
 const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
-console.log(process.env.PORT);
-console.log(process.env.ACCESS_KEY);
+// console.log(process.env.PORT);
+// console.log(process.env.ACCESS_KEY);
 
 //? asyncErrors to errorHandler:
 require('express-async-errors')
