5bd05312d00218d26c51690be17784d4ec1c1d76
DGKN <sakardogukan@gmail.com>
2023-11-25 21:50:03 +0300

    setToken.js added

---
+++ src/helpers/setToken.js
@@ -0,0 +1,19 @@
+"use strict"
+/* ---------------- SET TOKEN ----------------- */
+
+const jwt = require('jsonwebtoken')
+
+module.exports = function (userData, isRefresh = false) {
+    const data = {
+        access: userData.toJSON(),
+        refresh: { _id: userData._id, password: userData.password },
+        shortExpiresIn: '30m',
+        longExpiresIn: '3d'
+    }
+    return {
+        token: {
+            access: jwt.sign(data.access, process.env.ACCESS_KEY, { expiresIn: data.shortExpiresIn }),
+            refresh: (isRefresh ? null : jwt.sign(data.refresh, process.env.REFRESH_KEY, { expiresIn: data.longExpiresIn }))
+        }
+    }
+}
