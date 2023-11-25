2a6c75002e62e7e0097b9530f799c5eec2a5832a
DGKN <sakardogukan@gmail.com>
2023-11-25 21:44:35 +0300

    user.js changed

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,29 @@
 "use strict"
-/* ---------------------- USER MODEL ------------------- */
+/* ---------------- USER MODEL --------------- */
 
 const { mongoose } = require('../config/dbConnection')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
 
 const UserSchema = new mongoose.Schema({
     //id autoCreated
     username: {
         type: String,
-        required: true,
         trim: true,
-        unique: true
+        unique: [true, 'There is this username. Username filed must be unique'],
+        required: [true, 'Username filed must be required!']
     },
     password: {
         type: String,
-        required: true,
         trim: true,
+        required: true,
+        // set: (password) => password + '*123'
+        set: (password) => passwordEncrypt(password)
     },
     email: {
         type: String,
         trim: true,
         required: [true, 'Email filed must be required!'],
+        unique: [true, 'There is this email. Email filed must be unique'],
         validate: [
             (email) => email.includes('@') && email.includes('.'),
             'Email type is not correct!'
